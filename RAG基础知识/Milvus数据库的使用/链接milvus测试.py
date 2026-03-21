1c83694c0792c40b3f149fff41092ee4509d64ca
Dreamt <mochenge@163.com>
2026-03-21 21:57:27 +0800

    补充milvus连接示例

---
+++ RAG基础知识/Milvus数据库的使用/链接milvus测试.py
@@ -0,0 +1,19 @@
+from pymilvus import MilvusClient
+
+client = MilvusClient(
+    uri= "https://in03-aa3b3c45f217386.serverless.aws-eu-central-1.cloud.zilliz.com",
+    token = "db_aa3b3c45f217386:Zfl123456"
+   )
+
+databases = client.list_databases()
+print("数据库列表:", databases)
+
+#获取集合列表
+collections = client.list_collections()
+print("集合列表:", collections)
+
+collection = collections[0]
+
+#获取分区列表
+partitions = client.list_partitions(collection_name=collection)
+print("分区列表:", partitions)
